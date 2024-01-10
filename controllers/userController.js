936b61e1cb7635c8b4036382eddde8fb23ec3570
Diego Martinez <diegomartinez@MacBook-Air-de-Diego.local>
2024-01-10 21:09:49 +0100

    Adding in userControll delete and update functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,6 +28,27 @@ const userController = {
       res.status(404).send("User can´t be added");
     }
   },
+  deleteUser: async (req, res) => {
+    try {
+      const user = await User.findOneAndDelete(req.params.id);
+      res.json(user);
+    } catch (error) {
+      res.status(404).send("User can´t be deleted");
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate(
+        {
+          _id: req.params.id,
+        },
+        req.body
+      );
+      res.json(user);
+    } catch (error) {
+      res.status(404).send("User can´t be update");
+    }
+  },
 };
 
 module.exports = userController;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
 router.post("/", userController.addUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/:id", userController.updateUser);
 
 module.exports = router;
