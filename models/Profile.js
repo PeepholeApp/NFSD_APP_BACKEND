4db8388bae9544e3311ef65040ce190806a9a558
Maribel Alarcon <maribelalarcon@MacBook-Air-4.local>
2024-01-15 21:01:51 +0100

    modifique el modelo del profile

--- models/Profile.js
+++ models/Profile.js
@@ -4,16 +4,6 @@ const Schema = mongoose.Schema;
 const profileSchema = new Schema({
   name: { type: String, require: true, maxlenght: 15 },
   last_name: { type: String, require: false },
-  user_name: { type: String, unique: true, require: true },
-  email: {
-    type: String,
-    unique: true,
-    require: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
-  },
   dob: { type: Date, require: false },
   nationality: { type: String, require: true },
   gender: { type: String, require: true },
