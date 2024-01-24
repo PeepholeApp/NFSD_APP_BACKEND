d9cd3d68aa75d744aad8a4e4d88a4e6cd4b9ff9e
Maribel <maribelsoledadalarcon@gmail.com>
2024-01-24 11:58:18 +0100

    add changes in schema

--- models/Profile.js
+++ models/Profile.js
@@ -10,7 +10,7 @@ const profileSchema = new Schema({
   languages: { type: Array, require: true },
   photo: { type: [Array], require: true },
   bio: { type: String, require: true, maxlenght: 150 },
-  interest: { type: [Array], require: true },
+  // interest: { type: [Array], require: true },
 });
 
 const Profile = mongoose.model("profile", profileSchema);
