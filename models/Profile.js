749f35b6ee023634bcda0378137d60ba6ef95d52
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-12 16:00:05 +0100

    activate photo option

--- models/Profile.js
+++ models/Profile.js
@@ -9,7 +9,7 @@ const profileSchema = new Schema({
   nationality: { type: String, require: true },
   gender: { type: String, require: true },
   languages: { type: Array, require: true },
-  photo: { type: [Array], require: true },
+  photo: { type: Array, require: true },
   bio: { type: String, require: true, maxlenght: 150 },
   interest: { type: [Array], require: true },
 });
