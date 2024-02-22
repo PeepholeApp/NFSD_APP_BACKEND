8239edb794e27c16d227892e22e13f34c0e8a041
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-22 10:16:15 +0100

    Added  getConnection in controller

--- controllers/connectionsController.js
+++ controllers/connectionsController.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const Profile = require("../models/Profile");
 const { transporter } = require("../utils/nodemailer");
+const ConnectionRequest = require("../models/ConnectionRequest");
+const { json } = require("express");
 
 const Connections = {
   addConection: async (req, res) => {
@@ -13,6 +15,11 @@ const Connections = {
     const recipientUser = await User.findOne({ _id: recipientProfile.user });
     console.log("sender email to", recipientUser, senderUser, recipientProfile);
 
+    const newConnection = await ConnectionRequest.create({
+      sender: senderUser._id,
+      receiver: recipientUser._id,
+    });
+
     await transporter.sendMail({
       from: sender.email,
       to: recipientUser.email,
@@ -21,6 +28,27 @@ const Connections = {
     });
 
     res.status(200).send();
+  },
+
+  getConnection: async (req, res) => {
+    const profileId = req.params.profileId;
+    const sender = req.user; //usuario que esta logueado
+    const senderUser = await User.findOne({ email: sender.email });
+    const senderProfile = await Profile.findOne({ user: senderUser._id });
+
+    const recipientProfile = await Profile.findOne({ _id: profileId });
+    const recipientUser = await User.findOne({ _id: recipientProfile.user });
+
+    const connection = await ConnectionRequest.findOne({
+      sender: senderUser._id,
+      receiver: recipientUser._id,
+    });
+
+    if (connection) {
+      res.status(200).json(connection);
+    } else {
+      res.status(404).send("Connection not found");
+    }
   },
 };
 module.exports = Connections;
