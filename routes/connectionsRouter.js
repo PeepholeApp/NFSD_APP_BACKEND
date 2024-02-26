11fdeb7f2dcbf33dc6757cb1ad0dbf5355c5ba41
Maribel <maribelsoledadalarcon@gmail.com>
2024-02-26 15:20:28 +0100

    add router patch  and controller of  updateConnection

--- routes/connectionsRouter.js
+++ routes/connectionsRouter.js
@@ -6,5 +6,10 @@ const { verifyToken } = require("../controllers/userController");
 router.post("/:profileId", verifyToken, connectionsController.addConection);
 router.get("/:profileId", verifyToken, connectionsController.getConnection);
 router.get("/", verifyToken, connectionsController.getConnections);
+router.patch(
+  "/:profileId",
+  verifyToken,
+  connectionsController.updateConnection
+);
 
 module.exports = router;
