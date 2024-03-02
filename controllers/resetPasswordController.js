82281a11ffa7a9dca36a9431fbf0104198921d9d
knoyag <146126313+knoyag@users.noreply.github.com>
2024-03-03 00:37:00 +0100

    add nodemailers to controller

--- controllers/resetPasswordController.js
+++ controllers/resetPasswordController.js
@@ -1,55 +1,34 @@
-const nodemailer = require("nodemailer");
 const { verifyToken } = require("./userController");
 const crypto = require("crypto");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  auth: {
-    user: "", // Coloca tu usuario
-    pass: "", // Coloca tu contraseña
-  },
-});
+const nodemailerTransporter = require("../utils/nodemailer");
 
 const resetPasswordController = {
   sendResetEmail: async (req, res) => {
     try {
+      console.log("Reset password controller reached");
       const { email } = req.body;
 
       const tokenValid = await verifyToken(req, res, () => {});
 
       if (!tokenValid) {
-        return res
-          .status(401)
-          .json({ error: "Token not valid. Cannot initiate password reset." });
+        return res.status(401).json({ error: "Token not valid. Cannot initiate password reset." });
       }
 
       const resetToken = await generateResetToken();
 
       const mailOptions = {
-        from: "peepholeeee@gmail.com",
+        from: process.env.EMAIL_USER,
         to: email,
         subject: "Restablecimiento de Contraseña",
         html: `<p>Haz clic en el siguiente enlace para restablecer tu contraseña: <a href="http://localhost:3001/reset-password/${resetToken}">Restablecer Contraseña</a></p>`,
       };
 
-      await transporter.sendMail(mailOptions);
+      await nodemailerTransporter.transporter.sendMail(mailOptions);
 
-      return res
-        .status(200)
-        .json({
-          message: "Enviado con éxito.",
-        });
+      return res.status(200).json({ message: "Correo electrónico de restablecimiento enviado con éxito." });
     } catch (error) {
-      console.error(
-        "Error enviando el correo electrónico:",
-        error
-      );
-      return res
-        .status(500)
-        .json({
-          error: "Error",
-        });
+      console.error("Error enviando el correo electrónico de restablecimiento:", error);
+      return res.status(500).json({ error: "Error interno del servidor al enviar el correo electrónico." });
     }
   },
 };
