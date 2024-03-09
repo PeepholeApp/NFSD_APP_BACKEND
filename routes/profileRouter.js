40212a0de8dd995d8e6f4f581486d9d8ca123816
Diego Martinez <diegomrey1@gmail.com>
2024-03-09 19:58:34 +0100

    add role in request

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -4,7 +4,7 @@ const profileController = require("../controllers/profileController");
 
 router.get("/all", profileController.getAllProfiles);
 router.get("/user/:id", profileController.getProfileByUserId);
-router.get("/search/:name", profileController.getProfileByName);
+router.get("/search/:email", profileController.getProfileByEmail);
 router.get("/", profileController.getPaginationProfiles);
 router.post("/", profileController.addProfile);
 router.put("/:id", profileController.updateProfile);
