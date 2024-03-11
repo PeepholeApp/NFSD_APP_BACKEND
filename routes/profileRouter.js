5d62a382aa55fd0a62a3b51136e1269944dce3c8
Maribel <maribelsoledadalarcon@gmail.com>
2024-03-11 11:09:33 +0100

    added verifyToken en router profile

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken } = require("../controllers/userController");
 const profileController = require("../controllers/profileController");
 
+router.post("/", profileController.addProfile);
+router.get("/", profileController.getPaginationProfiles);
 router.get("/all", profileController.getAllProfiles);
 router.get("/user/:id", profileController.getProfileByUserId);
 router.get("/search/:name", profileController.getProfileByName);
-router.get("/", profileController.getPaginationProfiles);
-router.post("/", profileController.addProfile);
-router.put("/:id", profileController.updateProfile);
-router.delete("/:id", profileController.deleteProfile);
+router.put("/:id", verifyToken, profileController.updateProfile);
+router.delete("/:id", verifyToken, profileController.deleteProfile);
 
 module.exports = router;
